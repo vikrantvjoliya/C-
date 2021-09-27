1e9db41a936ec698d46260a3ac75ee4c7ea3e460
vikrantvjoliya <vikrant.v.joliya@gmail.com>
2021-09-27 22:34:20 +0530

    array rotation code in cpp

---
+++ array_rotation.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int i,a[i];
+	cin>>i;
+	//5 size of array
+	for(int k=0;k<i;k++){
+		cin>>a[k];
+		cout<<a[k]<<"\n";	
+	}
+	cout<<sizeof(a[i]);
+	
+	cout<<"Printing Array"<<endl;
+}
